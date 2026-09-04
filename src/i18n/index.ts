5d3b376a20a91d28f103b0e142a13a6dcf23d558
雨檐 <3458096571@qq.com>
2026-09-04 12:15:08 +0800

    Fix spacing in hero name string
    Removed space in greeting name string.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -13,7 +13,7 @@ const resources = {
       hero: {
         greeting: 'hello, world',
         cta: 'Say Hello',
-        name: "Hi, I'm Hiweny."
+        name: "Hi,I'm Hiweny"
       },
       section: {
         about: 'About',
